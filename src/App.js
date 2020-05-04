0d07772a3b1742b722c8e76826cb406e675f87ae
Shawn Tay <shawn@Shawns-MacBook-Pro.local>
2020-05-05 02:19:10 +0800

    remove unnecessary files and codes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Personality Matcher</h1>
+      <p>Is he/she your type?</p>
+      <button>Find out what is your</button>
     </div>
   );
 }
