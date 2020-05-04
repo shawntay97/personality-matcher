40ceeeb2fabd9e776d55b0e462963c772b96fb1a
Shawn Tay <shawn@Shawns-MacBook-Pro.local>
2020-05-05 01:55:00 +0800

    format

---
+++ src/knowledgeBase/mbtiData.js
@@ -0,0 +1,130 @@
+const mbtiData = {
+  INTJ: {
+    mbti: "INTJ",
+    role: "Analyst",
+    type: "Architect",
+    description:
+      "Imaginative and strategic thinkers, with a plan for everything.",
+    trait: "",
+  },
+  INTP: {
+    mbti: "INTP",
+    role: "Analyst",
+    type: "Logician",
+    description:
+      "Innovative inventors with an unquenchable thirst for knowledge.",
+    trait: "",
+  },
+  ENTJ: {
+    mbti: "ENTJ",
+    role: "Analyst",
+    type: "Commander",
+    description:
+      "Bold, imaginative and strong-willed leaders, always finding a way – or making one.",
+    trait: "",
+  },
+  ENTP: {
+    mbti: "ENTP",
+    role: "Analyst",
+    type: "Debator",
+    description:
+      "Smart and curious thinkers who cannot resist an intellectual challenge",
+    trait: "",
+  },
+  INFJ: {
+    mbti: "INFJ",
+    role: "Diplomat",
+    type: "Advocate",
+    description:
+      "Quiet and mystical, yet very inspiring and tireless idealists.",
+    trait: "",
+  },
+  INFP: {
+    mbti: "INFP",
+    role: "Diplomat",
+    type: "Mediator",
+    description:
+      "Poetic, kind and altruistic people, always eager to help a good cause.",
+    trait: "",
+  },
+  ENFJ: {
+    mbti: "ENFJ",
+    role: "Diplomat",
+    type: "Protagonist",
+    description:
+      "Charismatic and inspiring leaders, able to mesmerize their listeners.",
+    trait: "",
+  },
+  ENFP: {
+    mbti: "ENFP",
+    role: "Diplomat",
+    type: "Campaigner",
+    description:
+      "Enthusiastic, creative and sociable free spirits, who can always find a reason to smile.",
+    trait: "",
+  },
+  ISTJ: {
+    mbti: "ISTJ",
+    role: "Sentinels",
+    type: "Logistician",
+    description:
+      "Practical and fact-minded individuals, whose reliability cannot be doubted.",
+    trait: "",
+  },
+  ISFJ: {
+    mbti: "ISFJ",
+    role: "Sentinels",
+    type: "Defender",
+    description:
+      "Very dedicated and warm protectors, always ready to defend their loved ones.",
+    trait: "",
+  },
+  ESTJ: {
+    mbti: "ESTJ",
+    role: "Sentinels",
+    type: "Executive",
+    description:
+      "Excellent administrators, unsurpassed at managing things – or people.",
+    trait: "",
+  },
+  ESFJ: {
+    mbti: "ESFJ",
+    role: "Sentinels",
+    type: "Consul",
+    description:
+      "Extraordinarily caring, social and popular people, always eager to help.",
+    trait: "",
+  },
+  ISTP: {
+    mbti: "ISTP",
+    role: "Explorer",
+    type: "Virtuoso",
+    description:
+      "Bold and practical experimenters, masters of all kinds of tools.",
+    trait: "",
+  },
+  ISFP: {
+    mbti: "ISFP",
+    role: "Explorer",
+    type: "Adventurer",
+    description:
+      "Flexible and charming artists, always ready to explore and experience something new.",
+    trait: "",
+  },
+  ESTP: {
+    mbti: "ESTP",
+    role: "Explorer",
+    type: "Entrepreneur",
+    description:
+      "Smart, energetic and very perceptive people, who truly enjoy living on the edge.",
+    trait: "",
+  },
+  ESFP: {
+    mbti: "ESFP",
+    role: "Explorer",
+    type: "Entertainer",
+    description:
+      "pontaneous, energetic and enthusiastic people – life is never boring around them.",
+    trait: "",
+  },
+};
