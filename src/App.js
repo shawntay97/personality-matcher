8e6ab5cc16a71f7eac74fc72a3168a284b9de051
Shawn Tay <shawn@Shawns-MacBook-Pro.local>
2020-05-05 12:52:04 +0800

    add question card template

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import QuestionCard from "./components/QuestionCard";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,8 @@ function App() {
       <h1>Personality Matcher</h1>
       <p>Is he/she your type?</p>
       <button>Find out what is your</button>
+
+      <QuestionCard />
     </div>
   );
 }
---
+++ src/components/QuestionCard.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class QuestionCard extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Testing a question</h3>
+        <div>
+          <button>Option 1</button>
+          <button>Option 2</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default QuestionCard;
