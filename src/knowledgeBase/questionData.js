ac33be561bca5b6e8b46565b9a58562b74c8fdfc
Shawn Tay <shawn@Shawns-MacBook-Pro.local>
2020-05-05 11:26:03 +0800

    added question data

---
+++ src/knowledgeBase/questionData.js
@@ -0,0 +1,270 @@
+const questionData = [
+  // E-I Scale
+  {
+    question: null,
+    options: [
+      {
+        type: "extraversion",
+        answer: "Expand energy, enjoy groups",
+      },
+      {
+        type: "introversion",
+        answer: "Conserve energy, enjoy one-on-one",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "extraversion",
+        answer: "More outgoing, think out loud",
+      },
+      {
+        type: "introversion",
+        answer: "More reserved, think to yourself",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "extraversion",
+        answer: "Seek many tasks, public activities, interaction with others",
+      },
+      {
+        type: "introversion",
+        answer: "Seek private solitary activities with quiet to concentrate",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "extraversion",
+        answer: "external, communicative, express yourself",
+      },
+      {
+        type: "introversion",
+        answer: "internal, reticent, keep to yourself",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "extraversion",
+        answer: "active, initiate",
+      },
+      {
+        type: "introversion",
+        answer: "internal, reticent, keep to yourself",
+      },
+    ],
+  },
+  // S-N Scale
+  {
+    question: null,
+    options: [
+      {
+        type: "Sesnsing",
+        answer: "interpret literally",
+      },
+      {
+        type: "iNtuitive",
+        answer: "look for meaning and possibilities",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Sesnsing",
+        answer: "practical, realistic, experiential",
+      },
+      {
+        type: "iNtuitive",
+        answer: "imaginative, innovative, theoretical",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Sesnsing",
+        answer: "standard, usual, conventional",
+      },
+      {
+        type: "iNtuitive",
+        answer: "different, novel, unique",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Sesnsing",
+        answer: "focus on here-and-now",
+      },
+      {
+        type: "iNtuitive",
+        answer: "look to the future, global perspective, big picture”",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Sesnsing",
+        answer: 'facts, things, "what is"',
+      },
+      {
+        type: "iNtuitive",
+        answer: "ideas, dreams, “what could be,” philosophical",
+      },
+    ],
+  },
+  // T-F Scale
+  {
+    question: null,
+    options: [
+      {
+        type: "Thinking",
+        answer: "logical, thinking, questioning",
+      },
+      {
+        type: "Feeling",
+        answer: "empathetic, feeling, accommodating",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Thinking",
+        answer: "candid, straight forward, frank ",
+      },
+      {
+        type: "Feeling",
+        answer: "tactful, kind, encouraging",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Thinking",
+        answer: "firm, tend to criticize, hold the line",
+      },
+      {
+        type: "Feeling",
+        answer: "gentle, tend to appreciate, conciliate",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Thinking",
+        answer: "tough-minded, just",
+      },
+      {
+        type: "Feeling",
+        answer: "ender-hearted, merciful",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Thinking",
+        answer: "matter of fact, issue-oriented",
+      },
+      {
+        type: "Feeling",
+        answer: "sensitive, people-oriented, compassionate",
+      },
+    ],
+  },
+  // J-P Scale
+  {
+    question: null,
+    options: [
+      {
+        type: "Judging",
+        answer: "organized, orderly",
+      },
+      {
+        type: "Perceptive",
+        answer: "flexible, adaptable",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Judging",
+        answer: "plan, schedule",
+      },
+      {
+        type: "Perceptive",
+        answer: "unplanned, spontaneous",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Judging",
+        answer: "regulated, structured",
+      },
+      {
+        type: "Perceptive",
+        answer: "easygoing, “live” and “let live”",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Judging",
+        answer: "preparation, plan ahead",
+      },
+      {
+        type: "Perceptive",
+        answer: "go with the flow, adapt as you go",
+      },
+    ],
+  },
+  {
+    question: null,
+    options: [
+      {
+        type: "Judging",
+        answer: "control, govern",
+      },
+      {
+        type: "Perceptive",
+        answer: "latitude, freedom",
+      },
+    ],
+  },
+];
+
+//source: https://www.lusd.org/cms/lib6/CA01001399/Centricity/Domain/711/THE%20MYERS-BRIGGS.pdf
+
+export default questionData;
