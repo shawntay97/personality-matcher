9b66d26c535345d0e43bd7cd811ed2c6ce7b1a07
Shawn Tay <shawn@Shawns-MBP.lan>
2020-05-15 14:59:03 +0800

    Updates

---
+++ precache-manifest.3dce303757a5e09cfce2b268e81be7df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d704f12170466ccd19d84f3429c280c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fbd72d5b9f040841932",
+    "url": "/static/css/main.b4b0eaf1.chunk.css"
+  },
+  {
+    "revision": "c3c26415d67a09a832df",
+    "url": "/static/js/2.15d2bba1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15d2bba1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fbd72d5b9f040841932",
+    "url": "/static/js/main.69c1f3a3.chunk.js"
+  },
+  {
+    "revision": "6b2e6d78399414069a00",
+    "url": "/static/js/runtime-main.28006514.js"
+  }
+]);
